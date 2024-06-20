7696c07173f869633d9925afd923f0312b4b1a9a
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-20 15:48:17 +0200

    fix > added post content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = (post) => {
         <div className="card">
             <h2 className="cardTitle" >{post.post.title}</h2>
             <img className="cardImg"  src={post.post.image} alt={post.post.title} />
-            <p className="cardContent" >{post.content}</p>
+            <p className="cardContent" >{post.post.content}</p>
             <p className="cardContent" ><strong>Categoria:</strong>{post.post.category.name}</p>
             <p className="cardContent" ><strong>Tags:</strong></p>
             <p className="cardContent" >{post.post.published}</p>
